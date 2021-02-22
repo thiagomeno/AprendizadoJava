3580419360c5d10d157a813ff2825b993f1e76bc
thiagomeno <thiagomeno@gmail.com>
2021-02-21 23:42:57 -0300

    Implementação da class comparator

---
+++ Comparators/src/br/com/Exemplo01/EstudanteOrdemIdadeReversaCompator.java
@@ -0,0 +1,13 @@
+package br.com.Exemplo01;
+
+import java.util.Comparator;
+
+public class EstudanteOrdemIdadeReversaCompator implements Comparator<Estudantes>{
+
+    @Override
+    public int compare(Estudantes o1, Estudantes o2){
+        return o2.getIdade() - o1.getIdade()
+
+    }
+    
+}
